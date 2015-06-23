421bca84fd6c94e0e36d5dd6643f171c2673b4a4
Andrei Khoroshavin <horoshavin-andr@mail.ru>
2015-06-23 21:54:29 +0300

    create switch betweenn dota and cs:go forms

--- js/dragong_area.js
+++ js/dragong_area.js
@@ -1,9 +1,21 @@
 $(function(){
+	
+	// Open navigation menu on smartphones
 	$("#button_navigation").click(function(){
 		var menu=$("#navigation");
 		if(menu.css("display")=="none")
 			menu.css("display","block");
 		else
 			menu.css("display","none");
+	});
+
+	$("#switcher input").click(function(){
+		var name="#"+$(this).attr("id")+"_form";
+		var del_name="#"+$(this).parent().find("input:not(:checked)").attr("id")+"_form";
+		if($(name).css("display")!="block")
+		{
+			$(del_name).css("display", "none");
+			$(name).css("display", "block");
+		}
 	});
 });
