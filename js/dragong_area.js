aed0dcf198558a8ad5f3c847909ec49dcee021f2
Andrei Khoroshavin <horoshavin-andr@mail.ru>
2015-06-24 16:56:07 +0300

    fix switcher and panel with options, create list with items

--- js/dragong_area.js
+++ js/dragong_area.js
@@ -9,6 +9,7 @@ $(function(){
 			menu.css("display","none");
 	});
 
+	// Switcher between forms
 	$("#switcher input").click(function(){
 		var name="#"+$(this).attr("id")+"_form";
 		var del_name="#"+$(this).parent().find("input:not(:checked)").attr("id")+"_form";
@@ -18,4 +19,60 @@ $(function(){
 			$(name).css("display", "block");
 		}
 	});
+
+	// Draw Item's status line
+	var draw_lines=function(){
+		$(".exist_item .progress").each(function( index, element ){
+			
+			// Scan current value 
+			var num=parseInt($(element).next().text());
+			// Check value
+			if(num>100 || num<0)
+				return;
+			// Choose color
+			var color="progress-bar-";
+			if(num<40)
+				color+="success"; // Green Color
+			else if (num>75)
+				color+="danger"; // Red Color
+			else
+				color+="warning"; // Yellow Color
+			// Draw element
+			$(element).find(".progress-bar").css({"width": num+"%"}).addClass(color);
+		});
+	}
+
+	// Item Hover
+	$(".exist_item").hover(function(){
+		// hover ON
+		$(this).find(".progress").addClass('active');
+	},
+		// hover OFF
+	function(){
+		$(this).find(".progress").removeClass('active');
+	});
+
+	// Window Resize
+	$(window).resize(function(){
+		toOneColumn();
+	})
+
+	// Convert List to One Column on screen less than 611px
+	var toOneColumn=function(){
+		var width=$(window).width();
+		if( width < 611)
+			$("#items_list div.col-xs-6").each(function(index, element){
+				$(element).removeClass("col-xs-6");
+				$(element).addClass("col-xs-12");
+			});
+		else
+			$("#items_list div.col-xs-12").each(function(index, element){
+				$(element).removeClass("col-xs-12");
+				$(element).addClass("col-xs-6");
+			});
+	}
+
+	// Call by Default
+	draw_lines();
+	toOneColumn();
 });
