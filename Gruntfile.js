43eadd8958601ed688941f0c1a5b8654a4aba391
Andrei Khoroshavin <horoshavin-andr@mail.ru>
2015-06-27 13:54:50 -0400

    grunt structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,62 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+            files: [{
+                expand: true,
+                cwd: 'src/js/',
+                src: '*.js',
+                dest: 'build/js',
+                ext: '.min.js'
+          }]
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+        },
+        files: {
+          "src/css/authentication.css": "src/less/authentication.less",
+          "src/css/dragong_area.css": "src/less/dragong_area.less",
+        }
+      }
+    },
+    
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 9', 'Android > 0', 'bb > 0', 'Chrome > 0', 'Firefox > 0', 'Explorer > 0', 'iOS > 0', 'Opera > 0', 'Safari > 0', 'OperaMobile > 0', 'OperaMini > 0', 'ChromeAndroid > 0', 'FirefoxAndroid > 0', 'ExplorerMobile > 0'] 
+      },
+      your_target: {
+        files: {
+          "src/css/authentication.css": "src/css/authentication.css",
+          "src/css/dragong_area.css": "src/css/dragong_area.css",
+        } 
+      },
+    },
+
+    cssmin: {
+      target: {
+        files: {
+          "build/css/authentication.min.css": "src/css/authentication.css",
+          "build/css/dragong_area.min.css": "src/css/dragong_area.css"
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'less', 'autoprefixer', 'cssmin']);
 
 };
