cbc6fb407dc635758b2347e18f8f96cd392a2ac5
Andrei Khoroshavin <horoshavin-andr@mail.ru>
2015-07-05 11:23:34 -0400

    add fonts and .bowerrc file

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,5 +60,6 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['uglify', 'less', 'autoprefixer', 'cssmin']);
+  grunt.registerTask('cssprocess', ['less', 'autoprefixer', 'cssmin']);
 
 };
