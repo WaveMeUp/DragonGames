8190e177a98891abe64a5ba0a2e85f8c5f048151
Andrei Khoroshavin <horoshavin-andr@mail.ru>
2015-07-06 05:59:22 -0400

    add grunt option

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,5 +68,6 @@ module.exports = function(grunt) {
   // Default task(s).
   grunt.registerTask('default', ['concat', 'uglify', 'less', 'autoprefixer', 'cssmin']);
   grunt.registerTask('css', ['less', 'autoprefixer', 'cssmin']);
+  grunt.registerTask('js', ['concat', 'uglify']);
 
 };
